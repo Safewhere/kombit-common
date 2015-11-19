d5d022ae7006b3c82bfd35fabdf6c1071326d608
Le Tran Xuan Phuong <lxp@globeteam.com>
2015-11-19 08:40:23 -0800

    #13594 Re-configure all samples to work with projekt-stoettesystemerne.dk - .Net CH sample -- update sample error messages

--- KombitErrorMessages.cs
+++ KombitErrorMessages.cs
@@ -6,43 +6,18 @@
     public static class KombitErrorMessages
     {
         /// <summary>
-        ///     Common runtime error message
+        ///     Connection not found error message
         /// </summary>
-        public const string CommonRuntimeError = "CommonRuntimeError";
+        public const string ConnectionNotFoundError = "ConnectionNotFoundError";
 
         /// <summary>
-        ///     Connection resolution message
+        ///     Authentication failed error message
         /// </summary>
-        public const string ConnectionResolutionError = "ConnectionResolutionError";
+        public const string AuthenticationFailedError = "AuthenticationFailedError";
 
         /// <summary>
-        ///     Malformed request error message
+        ///     Bad request error message
         /// </summary>
-        public const string MalformedRequestError = "MalformedRequestError";
-
-        /// <summary>
-        ///     Path resolution error message
-        /// </summary>
-        public const string PathResolutionError = "PathResolutionError";
-
-        /// <summary>
-        ///     Audit user request error message
-        /// </summary>
-        public const string AuditUserRequestError = "AuditUserRequestError";
-
-        /// <summary>
-        ///     Not supported error message
-        /// </summary>
-        public const string NotSupportedException = "NotSupportedException";
-
-        /// <summary>
-        ///     Configuration error message
-        /// </summary>
-        public const string ConfigurationError = "ConfigurationError";
-
-        /// <summary>
-        ///     Database error message
-        /// </summary>
-        public const string DatabaseError = "DatabaseError";
+        public const string BadRequestError = "BadRequestError";
     }
 }
